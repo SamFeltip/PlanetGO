0f18398c50875bd1f20878b1d849f756caa1a7ef
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2022-11-17 16:42:20 +0000

    created landing page with sign up now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
   def home
   end
 
+  def landing
+    @example = 'sam felton'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # match '/users',   to: 'users#index',   via: 'get'
-
+  match '/welcome',     to: 'pages#landing',       via: 'get'
   # Defines the root path route ("/")
   root "pages#home"
 end
