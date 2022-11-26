19513d78cbfdac96dec8403237ddf5cb338eb5fa
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2022-11-26 23:16:34 +0000

    db migration and redundant test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_200342) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_162759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_200342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "is_on_landing_page"
+    t.integer "landing_page_position"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
