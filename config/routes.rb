7a5b2f52ad0ddf5a12f738f95463509cb7ba5b09
Adam Tomaszewski <attomaszewski1@sheffield.ac.uk>
2022-11-27 20:25:45 +0000

    Fix routes.rb to not remove changes on main

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # get 'users/index'
   devise_for :users
-  resources :users, :reviews, :interactions, :metrics
+  resources :users, :interactions, :metrics
+
+  resources :reviews do
+    member do
+      put 'like', to: 'reviews#like'
+      put 'unlike', to: 'reviews#unlike'
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # match '/users',   to: 'users#index',   via: 'get'
