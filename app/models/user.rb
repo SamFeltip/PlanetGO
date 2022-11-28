c3a1a009f86aa110a6ce4cf194f101b29c7c9bcd
Seth Roberts <sroberts6@sheffield.ac.uk>
2022-11-28 10:37:55 +0000

    Account locking

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :pwned_password
+         :pwned_password, :lockable, :trackable
 
   enum role: {
     user: 0,
