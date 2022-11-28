a84ab677673ce84c0ca6b8df229a4980f6594b97
Seth Roberts <sroberts6@sheffield.ac.uk>
2022-11-28 11:15:36 +0000

    User cannot edit themselves. Added info to 'Show Page'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  authorize_resource only: [:edit, :update, :destroy]
  
   def index
-    @users = User.all_except(current_user) # Don't include current user
+    @users = User.all
   end
 
   def show
