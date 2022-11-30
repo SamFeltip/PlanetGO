ce232a1c8b9b72f85ac89b2dfe5bfc9eaeef3001
Matthew Irvine <mirvine1@sheffield.ac.uk>
2022-11-30 11:13:13 +0000

    Small changes

--- spec/factories/faqs.rb
+++ spec/factories/faqs.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: faqs
+#
+#  id         :bigint           not null, primary key
+#  answer     :string
+#  question   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :faq do
     question { "MyString" }
