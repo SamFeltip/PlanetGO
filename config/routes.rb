c7fb7cfef24c9bd1c8dc06818c2d32431e5a3299
Seth Roberts <sroberts6@sheffield.ac.uk>
2022-11-30 12:33:55 +0000

    Removed ability to create users from index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   # get 'users/index'
+  match '/users/new', to: redirect('/404.html'), via: 'get' # No ability to create users without devise
   devise_for :users, :controllers => {:registrations_controller => "registrations_controller"}
-  resources :users, :reviews
+  resources :users, only: [:index, :show, :create, :edit, :destroy]
+  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # match '/users',   to: 'users#index',   via: 'get'
-
   # Defines the root path route ("/")
   # match '/users',   to: 'users#index',   via: 'get'
   match '/welcome',     to: 'pages#landing',       via: 'get'
   # Defines the root path route ("/")
   root "pages#landing"
-
 end
