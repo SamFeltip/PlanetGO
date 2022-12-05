235b63bf5ff8f6d768e49268808160a8b5385c06
acc19mi <mirvine1@sheffield.ac.uk>
2022-12-05 16:13:04 +0000

    small changes to nav bar, FAQs links and faq page

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -4,11 +4,11 @@
 #
 #  id         :bigint           not null, primary key
 #  answer     :string
+#  answered   :boolean          default(FALSE)
+#  displayed  :boolean          default(FALSE)
 #  question   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  answered   :boolean          default(FALSE)
-#  displayed   :boolean          default(FALSE)
 #
 class Faq < ApplicationRecord
 end
