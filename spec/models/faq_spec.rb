a330cc84e3e75f82325891f192ca3b6d5cfbf699
acc19mi <mirvine1@sheffield.ac.uk>
2022-12-07 16:26:36 +0000

    Updated faq db to revert some changes

--- spec/models/faq_spec.rb
+++ spec/models/faq_spec.rb
@@ -4,6 +4,8 @@
 #
 #  id         :bigint           not null, primary key
 #  answer     :string
+#  answered   :boolean          default(FALSE)
+#  displayed  :boolean          default(FALSE)
 #  question   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
