1eb3d52fbd3532c234592d3d985657d64a96a3dd
Adam Tomaszewski <attomaszewski1@sheffield.ac.uk>
2022-12-07 16:41:00 +0000

    metrics graph works for the three general metrics. Do not pair large date...

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,5 +1,22 @@
 import Rails from "@rails/ujs";
-import "bootstrap"
+import "bootstrap";
 import '../scripts/collecting_metrics';
+import '../scripts/jquery-jvectormap-world-merc';
+import Chart from 'chart.js/auto';
 
 Rails.start();
+
+window.onload = () => {
+  if (window.location.pathname == "/metrics") {
+    // Setup the chart
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: JSON.parse(ctx.canvas.dataset.labels),
+        datasets:
+          JSON.parse(ctx.canvas.dataset.data)
+      },
+    });
+  }
+}
