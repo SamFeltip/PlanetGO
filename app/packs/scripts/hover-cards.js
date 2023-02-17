6ccdbce260ac1a8d8ae6f257d2bed031a2c9bbbe
Urszula Talalaj <utalalaj1@sheffield.ac.uk>
2023-02-17 15:58:08 +0000

    Use a relative URL

--- app/packs/scripts/hover-cards.js
+++ app/packs/scripts/hover-cards.js
@@ -1,25 +1,14 @@
-console.log("hello world");
-// Select the card-hover elements
-var elements = document.querySelectorAll('.card-hover');
+var elements = document.querySelectorAll(".card-hover");
 
-// Iterate over each element
 for (const element of elements) {
-    console.log("iterating on elements")
-    // Add a click event listener to the element
-    element.addEventListener('click', function() {
-        // Handle the click event
-        console.log("clicked!")
-        // Select the element with the class 'my-class' that is inside the div element
-        const header = element.querySelector('.card-header');
+    element.addEventListener("click", function () {
+        const header = element.querySelector(".card-header");
 
         const interest_urls = {
-            "Basic": "/pricings/basic/register_interests/new",
-            "Premium": "http://0.0.0.0:3000/pricings/premium/register_interests/new",
-            "Premium+": "http://0.0.0.0:3000/pricings/premium_plus/register_interests/new"
-        }
-
-        // Print the text within the element
-        console.log(header.innerText);
+            Basic: "/pricings/basic/register_interests/new",
+            Premium: "/pricings/premium/register_interests/new",
+            "Premium+": "/pricings/premium_plus/register_interests/new",
+        };
 
         window.location.href = interest_urls[header.innerText];
     });
