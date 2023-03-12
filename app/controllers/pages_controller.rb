cf71520e09284e11bd92228f9514a59ba2345e0b
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-03-12 13:40:58 +0000

    show "friends" in myaccount (todo: make this actually show friends and not just first 5 users)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,15 @@
 
 class PagesController < ApplicationController
   def home; end
+  before_action :authenticate_user!, only: %i[account]
 
   def landing
     @special_reviews = Review.where(is_on_landing_page: 'true').order(:landing_page_position)
   end
 
   def account
+    # TODO make this actually my friends
+    @friends = User.where("id < 5")
 
     @future_outings = Outing.future_outings(current_user)
     @past_outings = Outing.past_outings(current_user)
