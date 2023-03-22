d1b2728f66b1b51523259d7f14fd3a7c00759b2e
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-03-22 10:58:48 +0000

    testing pending_events functions

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -24,15 +24,54 @@
 #
 require 'rails_helper'
 
-# RSpec.describe Event do
-#   let!(:event_creator) { create(:user) }
-#   let(:created_event) { create(:event, user_id: event_creator) }
-#   # let!(:other_event_creator) { create(:user) }
-#
-#   describe '#pending_events' do
-#     specify 'returns all pending events' do
-#       events = described_class.my_pending_events(event_creator)
-#       expect(events.count).to eq(1)
-#     end
-#   end
-# end
+RSpec.describe Event do
+  let(:event_creator) { create(:user) }
+  let(:other_event_creator) { create(:user) }
+
+  let!(:my_event) { create(:event, user_id: event_creator.id) }
+  let!(:other_event) { create(:event, user_id: other_event_creator.id) }
+
+
+  describe '#my_pending_events' do
+
+    context 'with my events' do
+      let!(:my_approved_event) { create(:event, approved: true, user_id: event_creator.id) }
+
+      it 'does not include my approved events' do
+        expect(described_class.my_pending_events(event_creator)).to_not include(my_approved_event)
+      end
+
+      it 'returns all my pending events' do
+        expect(described_class.my_pending_events(event_creator)).to include(my_event)
+      end
+    end
+
+    context 'with other events' do
+      it 'does not include other pending events' do
+        expect(described_class.my_pending_events(event_creator)).to_not include(other_event)
+      end
+    end
+  end
+
+  describe '#other_users_pending_events' do
+
+    context 'with my events' do
+      it 'does not include my pending events' do
+        expect(described_class.other_users_pending_events(event_creator)).to_not include(my_event)
+      end
+    end
+
+    context 'with other events' do
+      let!(:other_approved_event) { create(:event, approved: true, user_id: other_event_creator.id) }
+
+      it 'returns all other pending events' do
+        expect(described_class.other_users_pending_events(event_creator)).to include(other_event)
+      end
+
+      it 'does not include approved events' do
+        expect(described_class.other_users_pending_events(event_creator)).to_not include(other_approved_event)
+      end
+    end
+
+  end
+end
