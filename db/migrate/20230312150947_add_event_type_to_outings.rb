a7352ec7d10e860cb2fe1859195f3d8e45253303
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-03-28 17:13:35 +0100

    added to seeds

--- db/migrate/20230312150947_add_event_type_to_outings.rb
+++ db/migrate/20230312150947_add_event_type_to_outings.rb
@@ -2,9 +2,7 @@
 
 class AddEventTypeToOutings < ActiveRecord::Migration[7.0]
   def change
-    change_table :outings, bulk: true do |t|
-      t.add_column :event_type, :integer
-      t.add_column :invitation_token, :bigint
-    end
+    add_column :outings, :event_type, :integer
+    add_column :outings, :invitation_token, :bigint
   end
 end
