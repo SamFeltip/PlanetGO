094b63a15e7ce2602768b4024939ade094a4c1ca
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-03-31 13:32:26 +0100

    bring some changes made in add-events branch into this branch

--- app/packs/scripts/outings.js
+++ app/packs/scripts/outings.js
@@ -1,23 +1,2 @@
 console.log("outings js loaded")
 
-// when #outing-footer-next is clicked, check if content-who or when is visible
-// if who is visible, show when
-$(function() {
-    $('#outing-footer-next').click(function() {
-        console.log("next pressed");
-        if ($('#content-who').is(":visible")) {
-            show_when_content();
-        }
-    });
-});
-
-// the same but who and when are reversed, and it inspects #outing-footer-prev
-$(function() {
-    $('#outing-footer-prev').click(function() {
-        console.log("prev pressed");
-        if ($('#content-when').is(":visible")) {
-            show_who_content();
-        }
-    });
-});
-
