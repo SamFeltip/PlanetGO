3ff6156897bf4d899136f23405ee69b50ada5b6a
Urszula Talalaj <utalalaj1@sheffield.ac.uk>
2023-04-24 12:28:55 +0100

    Add location to event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,10 +5,16 @@
 # Table name: events
 #
 #  id            :bigint           not null, primary key
+#  address_line1 :string
+#  address_line2 :string
 #  approved      :boolean
 #  description   :text
+#  latitude      :float
+#  longitude     :float
 #  name          :string
+#  postcode      :string
 #  time_of_event :datetime
+#  town          :string
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  category_id   :bigint
@@ -17,6 +23,8 @@
 # Indexes
 #
 #  index_events_on_category_id  (category_id)
+#  index_events_on_latitude     (latitude)
+#  index_events_on_longitude    (longitude)
 #  index_events_on_user_id      (user_id)
 #
 # Foreign Keys
@@ -33,7 +41,7 @@ RSpec.describe Event do
   let!(:category) { Category.create(name: 'Bar') }
   let!(:category2) { Category.create(name: 'SomethingUnusable') }
   let!(:my_event) { create(:event, user_id: event_creator.id, category_id: category.id) }
-  let!(:other_event) { create(:event, user_id: other_event_creator.id, category_id: category2.id) }
+  let!(:other_event) { create(:event, user_id: other_event_creator.id, category_id: category2.id, address_line2: 'City Centre') }
 
   describe '#image_path' do
     it 'returns a path to an image corresponding with the category name' do
@@ -42,6 +50,16 @@ RSpec.describe Event do
 
     it 'returns a path to the unknown image when no category is available' do
       expect(other_event.image_path).to eq 'event_images/unknown.png'
+    end
+  end
+
+  describe '#address' do
+    it 'returns a formatted string when address_line2 not present' do
+      expect(my_event.address).to eq '104 West Street, Sheffield, S1 4EP'
+    end
+
+    it 'returns a formatted string when address_line2 present' do
+      expect(other_event.address).to eq '104 West Street, City Centre, Sheffield, S1 4EP'
     end
   end
 
