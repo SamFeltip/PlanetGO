b1c373cace79ed0e4c1495b6dc7cd148ea1e1472
acc19mi <mirvine1@sheffield.ac.uk>
2023-05-07 15:46:48 +0100

    Removed participants pages, changed route slightly. Manual testing indicated no problems.

--- config/routes.rb
+++ config/routes.rb
@@ -29,13 +29,11 @@ Rails.application.routes.draw do
       get 'set_details'
       post 'send_invites'
       post 'stop_count'
-      resources :participants, only: %i[new create]
+      resources :participants, only: %i[new create invite destroy update]
       resource :invite_link, only: :show
     end
     post :send_invites, on: :member
   end
-
-  resources :participants
 
   # No ability to create users without devise
   get '/users/new', to: redirect('/404.html')
