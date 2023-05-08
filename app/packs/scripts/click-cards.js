c99ff7e4198b8f7bc38e24dac88aa5f48f3f3c0b
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-05-08 17:00:14 +0100

    smarter click cards now work with outings

--- app/packs/scripts/click-cards.js
+++ app/packs/scripts/click-cards.js
@@ -1,20 +1,16 @@
 let click_cards = document.querySelectorAll(".click-card");
 
 for (const click_card of click_cards) {
-    let click_card_body = click_card.querySelector(".body");
-    let click_card_header = click_card.querySelector(".header");
+    let clickables = click_card.querySelectorAll(".clickable");
     let click_card_url = click_card.getAttribute("data-url")
 
-    // if there is a body, add event listener to first click card body element
-    if (click_card_body) {
-        click_card_body.addEventListener("click", function () {
-            window.location.href = `${click_card_url}`;
-        });
-    }
+    for (const clickable of clickables) {
+        // if there is a body, add event listener to first click card body element
+        if (clickable) {
+            clickable.addEventListener("click", function () {
+                window.location.href = `${click_card_url}`;
+            });
+        }
 
-    if (click_card_header) {
-        click_card_header.addEventListener("click", function () {
-            window.location.href = `${click_card_url}`;
-        });
     }
 }
