e96aa7d986e3730fb4e2925cb2f8e3774c41f5d7
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-05-09 14:58:30 +0100

    more clear proposed event time setting

--- app/decorators/proposed_event_decorator.rb
+++ app/decorators/proposed_event_decorator.rb
@@ -4,8 +4,8 @@ class ProposedEventDecorator < ApplicationDecorator
   delegate_all
 
   def proposed_datetime(compact: true)
-    return 'No time selected' if object.proposed_datetime.nil?
     return object.event.decorate.display_time if object.event.time_of_event.present?
+    return 'No time selected' if object.proposed_datetime.nil?
 
     return object.proposed_datetime.strftime('%b %d, %H:%M') if compact
 
@@ -13,6 +13,6 @@ class ProposedEventDecorator < ApplicationDecorator
   end
 
   def vote_likes
-    "#{votes_for.size} likes"
+    "#{votes_for.size} votes"
   end
 end
