deac11e2c60e26a8b371d12297b4a468f4a01dc0
Adam Tomaszewski <attomaszewski1@sheffield.ac.uk>
2023-05-11 10:04:15 +0100

    fixed tests

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -34,23 +34,23 @@ RSpec.describe Metric do
     end
 
     it 'selecting datetime with no events happening returns list with data full of zeros' do
-      expect(handle_graph_category('2023-2-20', '2023-2-22', 'Day', 'Sport')).to eq(['["2023-02-20","2023-02-21"]', '[{"label":"Sport","data":[0,0]}]'])
+      expect(handle_graph_category('2023-2-20', '2023-2-22', 'Day', 'Music')).to eq(['["2023-02-20","2023-02-21"]', '[{"label":"Music","data":[0,0]}]'])
     end
 
     it 'selecting datetime with 2 events happening return list with data containing two ones' do
-      expect(handle_graph_category('2023-2-23', '2023-2-25', 'Day', 'Sport')).to eq(['["2023-02-23","2023-02-24"]', '[{"label":"Sport","data":[1,1]}]'])
+      expect(handle_graph_category('2023-2-23', '2023-2-25', 'Day', 'Music')).to eq(['["2023-02-23","2023-02-24"]', '[{"label":"Music","data":[1,1]}]'])
     end
 
     it 'selecting datetime with 2 events happening but a different event name return list with data of zeros' do
-      expect(handle_graph_category('2023-2-23', '2023-2-25', 'Day', 'Sport2')).to eq(['["2023-02-23","2023-02-24"]', '[{"label":"Sport2","data":[0,0]}]'])
+      expect(handle_graph_category('2023-2-23', '2023-2-25', 'Day', 'Sport')).to eq(['["2023-02-23","2023-02-24"]', '[{"label":"Sport","data":[0,0]}]'])
     end
 
     it 'selecting datetime with 3 events happening on two separate months using the Month timescale returns data containing a two and a one' do
-      expect(handle_graph_category('2023-1-1', '2023-3-1', 'Month', 'Sport')).to eq(['["2023-01-01","2023-02-01"]', '[{"label":"Sport","data":[1,2]}]'])
+      expect(handle_graph_category('2023-1-1', '2023-3-1', 'Month', 'Music')).to eq(['["2023-01-01","2023-02-01"]', '[{"label":"Music","data":[1,2]}]'])
     end
 
     it 'selecting datetime with 3 events happening on two separate months using the Month timescale and the name All returns data containing a two and a one' do
-      expect(handle_graph_category('2023-1-1', '2023-3-1', 'Month', 'All')).to eq(['["2023-01-01","2023-02-01"]', '[{"label":"Sport","data":[1,2]}]'])
+      expect(handle_graph_category('2023-1-1', '2023-3-1', 'Month', 'All')).to eq(['["2023-01-01","2023-02-01"]', '[{"label":"Music","data":[1,2]}]'])
     end
   end
 end
