9f4ef2b219b08a76dc38c062752d9437be426d0b
Adam Tomaszewski <attomaszewski1@sheffield.ac.uk>
2023-05-11 09:48:13 +0100

    merge resolve

--- app/helpers/d_b_queries.rb
+++ app/helpers/d_b_queries.rb
@@ -21,9 +21,9 @@ module DBQueries
   end
 
   def get_event_category_popularity(name, start_date_time, end_date_time)
-    ProposedEvent.joins(event: :category).where('categories.name = :name and proposed_events.proposed_datetime >=
-                                                :start_date_time and proposed_events.proposed_datetime < :end_date_time',
-                                                { name:, start_date_time:, end_date_time: }).count
+    search_string = 'categories.name = :name and proposed_events.proposed_datetime >= :start_date_time and proposed_events.proposed_datetime < :end_date_time',
+                    { name:, start_date_time:, end_date_time: }
+    ProposedEvent.joins(event: :category).where(search_string).count
   end
 
   def get_average_time_spent(metrics, start_date_time, end_date_time)
